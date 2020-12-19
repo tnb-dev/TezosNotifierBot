37ac2f44799b0f2e8cff1c76eede1e84e334aa66
Alexey Shvorak <a.shvorak@basis.center>
2020-12-19 11:07:56 +0300

    Added Baken`n`Rolls emoji

---
+++ TezosNotifyBot.Storage/Migrations/20201219080742_AddBakenRollsEmoji.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TezosNotifyBot.Storage.Migrations
+{
+    public partial class AddBakenRollsEmoji : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "address_config",
+                columns: new[] { "id", "icon" },
+                values: new object[] { "tz1NortRftucvAkD1J58L32EhSVrQEWJCEnB", "🥨" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "address_config",
+                keyColumn: "id",
+                keyValue: "tz1NortRftucvAkD1J58L32EhSVrQEWJCEnB");
+        }
+    }
+}
