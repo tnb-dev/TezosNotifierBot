b31d4b02f14928ad98a031c1122454a678f49c5f
Максим Федоров <makstl@mail.ru>
2020-12-28 18:42:31 +0300

    Check tzkt indexer head

---
+++ TezosNotifyBot/Tzkt/Head.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TezosNotifyBot.Tzkt
+{
+	public class Head
+	{
+        public int level { get; set; }
+        public string hash { get; set; }
+        public string protocol { get; set; }
+        public DateTime timestamp { get; set; }
+        public int votingEpoch { get; set; }
+        public int votingPeriod { get; set; }
+        public int knownLevel { get; set; }
+        public DateTime lastSync { get; set; }
+        public bool synced { get; set; }
+        public int quoteLevel { get; set; }
+        public double quoteBtc { get; set; }
+        public double quoteEur { get; set; }
+        public double quoteUsd { get; set; }
+    }
+}
---
+++ TezosNotifyBot/Tzkt/TzKtClient.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Net;
+
+namespace TezosNotifyBot.Tzkt
+{
+	public class TzKtClient : ITzKtClient
+	{
+		ILogger<TzKtClient> _logger;
+		WebClient _client;
+		public TzKtClient(ILogger<TzKtClient> logger, string url)
+		{
+			_client = new WebClient();
+			_client.BaseAddress = url;
+			_logger = logger;
+		}
+
+		Head ITzKtClient.GetHead()
+		{
+			string head = Download("v1/head");
+			return JsonConvert.DeserializeObject<Head>(head);
+		}
+
+		string Download(string addr)
+		{
+			try
+			{
+				_logger.LogDebug($"download {_client.BaseAddress}/{addr}");
+				var result = _client.DownloadString(addr);
+				_logger.LogDebug($"download complete: {_client.BaseAddress}/{addr}");
+				return result;
+			}
+			catch (Exception e)
+			{
+				_logger.LogError(e, "Error downloading from {_client.BaseAddress}/{addr}");
+				throw;
+			}
+		}
+	}
+}
