cb3cedb8ca817014632fcd5d1882a0a4c72dfd34
Alexey Shvorak <a.shvorak@basis.center>
2020-12-29 05:38:59 +0300

    DevOps. Fix user_address name column

---
+++ TezosNotifyBot.Storage/Migrations/20201229023823_FixUserAddressName.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TezosNotifyBot.Storage.Migrations
+{
+    public partial class FixUserAddressName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "name",
+                table: "user_address",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "name",
+                table: "user_address",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldDefaultValue: "");
+        }
+    }
+}
