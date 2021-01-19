34f0fa05a587ffb403867470de6f7663a16e479d
Максим Федоров <makstl@mail.ru>
2021-01-19 15:54:03 +0300

    View tokens balances

---
+++ TezosNotifyBot/BetterCallDev/IBetterCallDevClient.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TezosNotifyBot.BetterCallDev
+{
+	public interface IBetterCallDevClient
+	{
+		Account GetAccount(string address);
+	}
+}
