a80b12b686745731ec85907347ed4c94baa2d1d4
Максим Федоров <maks.gm@gmail.com>
2021-06-03 17:12:53 +0300

    fix

--- TezosNotifyBot/TezosBot.cs
+++ TezosNotifyBot/TezosBot.cs
@@ -2609,7 +2609,8 @@ namespace TezosNotifyBot
                         welcomeBack = true;
                         NotifyUserActivity("🤗 User " + UserLink(user) + " is back");
                     }
-
+                    if (message.Text.StartsWith("/now"))
+                        SendTextMessage(user.Id, DateTime.Now.ToString("d MMMMM HH:mm", CultureInfo.GetCultureInfo("ru")));
                     if (message.Text.StartsWith("/start"))
                     {
                         if (newUser || welcomeBack)
