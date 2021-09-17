bffae6460ac90caeaf915696bb2c6748ab257105
Максим Федоров <maks.gm@gmail.com>
2021-09-17 15:47:35 +0300

    fix

--- TezosNotifyBot/TezosBot.cs
+++ TezosNotifyBot/TezosBot.cs
@@ -1995,7 +1995,7 @@ namespace TezosNotifyBot
                 if (fr == null)
                     continue;
                 fr.Reverse();
-                long freeBalance = (long)(addrMgr.GetDelegate(_nodeManager.Client, block.Hash, addr, true).balance * 1000000M)
+                long freeBalance = (long)addrMgr.GetDelegate(_nodeManager.Client, block.Hash, addr, true).balance
                     - fr[6].futureBlockDeposits - fr[6].futureEndorsementDeposits;
                 for (int c = 7; c < 12; c++)
                 {
