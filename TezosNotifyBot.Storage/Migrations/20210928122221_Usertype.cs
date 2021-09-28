859afe733b485f19177552dcdd8c3f57361de8f9
Максим Федоров <maks.gm@gmail.com>
2021-09-28 15:22:40 +0300

    usertype

---
+++ TezosNotifyBot.Storage/Migrations/20210928122221_Usertype.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TezosNotifyBot.Storage.Migrations
+{
+    public partial class Usertype : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "type",
+                table: "user",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "type",
+                table: "user");
+        }
+    }
+}
