79667b06458afecbe38bfa8fa1b19b1cd4c1db5d
Максим Федоров <maks.gm@gmail.com>
2021-09-30 14:19:20 +0300

    fix

--- TezosNotifyBot/TezosBot.cs
+++ TezosNotifyBot/TezosBot.cs
@@ -1992,7 +1992,7 @@ namespace TezosNotifyBot
             foreach (var addr in delegates)
             {
                 var fr = tzKtClient.GetBakerFutureRewards(addr);
-                if (fr == null)
+                if (fr == null || fr.Count < 12)
                     continue;
                 fr.Reverse();
                 long freeBalance = (long)addrMgr.GetDelegate(_nodeManager.Client, block.Hash, addr, true).balance
