194ba07b5eab6e0ec3762b9aee7cf8f0c78e9da4
Максим Федоров <maks.gm@gmail.com>
2021-10-01 16:29:16 +0300

    Omni channel fix

--- TezosNotifyBot/TezosBot.cs
+++ TezosNotifyBot/TezosBot.cs
@@ -3590,7 +3590,7 @@ namespace TezosNotifyBot
                     bool newChat = !repo.UserExists(message?.Chat.Id ?? update.ChannelPost.Chat.Id);
                     var chat = message?.Chat ?? update.ChannelPost.Chat;
                     var from = message?.From ?? update.ChannelPost.From;
-                    if (from == null || from.IsBot)
+                    if (from?.IsBot ?? false)
                         return;
                     int messageId = message?.MessageId ?? update.ChannelPost.MessageId;
                     string messageText = message?.Text ?? update.ChannelPost.Text;
