87d2b7920c794ece7b61f873d3a85bc93393b48c
Alexey Shvorak <alex@shvorak.com>
2021-10-13 22:33:22 +0300

    TNB-32. Disable network_issue_notify for all users

---
+++ TezosNotifyBot.Storage/Migrations/20211013193103_DisableNetworkIssueNotify.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TezosNotifyBot.Storage.Migrations
+{
+    public partial class DisableNetworkIssueNotify : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE \"user\" SET network_issue_notify = 0 WHERE true;");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
