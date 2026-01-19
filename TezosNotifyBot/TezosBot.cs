571dd1f56a1f3c5d27a5eb9c3450d96165adb3a3
Максим Федоров <maks.gm@gmail.com>
2026-01-19 20:45:54 +0300

    Check max user address count

--- TezosNotifyBot/TezosBot.cs
+++ TezosNotifyBot/TezosBot.cs
@@ -1441,16 +1441,19 @@ namespace TezosNotifyBot
                 if (ci != null)
                 {
 					// Check max user address count
-					var addrCount = db.UserAddresses.Count(x => x.UserId == user.Id);
-					var limit = user.MaxAddrCount ?? Config.MaxAddressCount;
-					if (addrCount >= limit)
+					if (!db.UserAddresses.Any(x => x.UserId == user.Id && !x.IsDeleted && x.Address == addr))
 					{
-						string maxAddrReached = $"👛 You’ve reached the limit of {limit} tracked addresses.\r\n\r\nIf you’d like to increase this limit, please contact our support team.";
-						if (chat == null)
-							await SendTextMessage(db, user.Id, maxAddrReached, ReplyKeyboards.ContactSupport(user));
-						else
-							await SendTextMessage(chat.Id, maxAddrReached);
-						return;
+						var addrCount = db.UserAddresses.Count(x => x.UserId == user.Id && !x.IsDeleted);
+						var limit = user.MaxAddrCount ?? Config.MaxAddressCount;
+						if (addrCount >= limit)
+						{
+							string maxAddrReached = $"👛 You’ve reached the limit of {limit} tracked addresses.\r\n\r\nIf you’d like to increase this limit, please contact our support team.";
+							if (chat == null)
+								await SendTextMessage(db, user.Id, maxAddrReached, ReplyKeyboards.ContactSupport(user));
+							else
+								await SendTextMessage(chat.Id, maxAddrReached);
+							return;
+						}
 					}
 
                     decimal bal = ci.balance / 1000000M;
